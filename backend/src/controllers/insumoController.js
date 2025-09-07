27959756c864f9351d12ddfad46e1a36728a7882
Gustavo Crispim <gustavocrispim2@gmail.com>
2025-09-07 18:13:25 -0300

    feat: adicionar rotas e controllers para atualizar e deletar insumos

--- backend/src/controllers/insumoController.js
+++ backend/src/controllers/insumoController.js
@@ -1,5 +1,6 @@
 const Insumo = require("../models/insumo");
 
+// adicionar insumos
 const addInsumo = async (req, res) => {
   const { nome, quantidade, unidadeMedida } = req.body;
 
@@ -20,6 +21,57 @@ const addInsumo = async (req, res) => {
   }
 };
 
+//Listar insumos
+const getAllInsumos = async (req, res) => {
+  try {
+    const insumos = await Insumo.find();
+    res.status(200).json(insumos);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Erro ao buscar insumo", error: error.message });
+  }
+};
+
+// Controlador para atualizar um insumo existente
+const updateInsumo = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const insumo = await Insumo.findByIdAndUpdate(id, req.body, { new: true });
+
+    if (!insumo) {
+      return res.status(404).json({ message: "Insumo não encontrado" });
+    }
+
+    res.status(200).json(insumo);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Erro ao atualizar insumo", error: error.message });
+  }
+};
+
+// Controlador para deletar um insumo
+const deleteInsumo = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const insumo = await Insumo.findByIdAndDelete(id);
+
+    if (!insumo) {
+      return res.status(404).json({ message: "Insumo não encontrado" });
+    }
+
+    res.status(200).json({ message: "Insumo removido com sucesso" });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Erro ao remover insumo", error: error.message });
+  }
+};
+
 module.exports = {
   addInsumo,
+  getAllInsumos,
+  updateInsumo,
+  deleteInsumo,
 };
--- backend/src/routes/insumoRoutes.js
+++ backend/src/routes/insumoRoutes.js
@@ -1,9 +1,20 @@
 // backend/src/routes/insumoRoutes.js
 const express = require("express");
 const router = express.Router();
-const { addInsumo } = require("../controllers/insumoController");
+const {
+  addInsumo,
+  getAllInsumos,
+  updateInsumo,
+  deleteInsumo,
+} = require("../controllers/insumoController");
 
 // Rota para adicionar um novo insumo
 router.post("/", addInsumo);
+
+router.get("/", getAllInsumos);
+
+router.put("/:id", updateInsumo);
+
+router.delete("/:id", deleteInsumo);
 
 module.exports = router;
